4d4b772455567e234869a26bc97d6ebb3f76bd84
gresba <kimpaul0709@gmail.com>
2022-08-05 17:13:43 -0400

    Cmd prompt finished

--- main.py
+++ main.py
@@ -10,6 +10,27 @@ class BaseModel(Model):
 
 class Card(BaseModel):
     title = CharField()
-    back = CharField()
+    content = CharField()
     correct = IntegerField()
     incorrect = IntegerField()
+
+db.drop_tables([Card])
+db.create_tables([Card])
+
+def makeCard():
+    title = input('Title of Card')
+    content = input('Content of Card')
+    card = Card(title='f{title}', content='f{content}')
+    card.save()
+
+def startGame(amountOfCards):
+    cards = Card.select().limit(amountOfCards)
+    print(cards)
+
+choice = input('Make a card or test knowledge?`\n1 - Make a card\n2 - Test knowledge\nChoice: ')
+
+if choice == "1":
+    makeCard()
+elif choice == "2":
+    amountOfCards = int(input('How many cards would you like to study? '))
+    startGame(amountOfCards)
